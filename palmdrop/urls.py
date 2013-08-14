482224ee2be5943ab1fd6f09d4140173305299c6
Michael Petychakis <mpetyx@gmail.com>
2013-08-14 13:04:41 +0300

    enabling admin

--- palmdrop/urls.py
+++ palmdrop/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin

1c8d95dde047c19f02e11ae183128aa5deb57b26
Michael Petychakis <mpetyx@gmail.com>
2013-09-09 00:03:05 +0300

    changing root path

--- palmdrop/settings.py
+++ palmdrop/settings.py
@@ -109,7 +109,7 @@ MEDIA_ROOT = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath(os.path.dirname(__file__)) + '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
